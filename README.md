625c64ad54ef88aa4110556bdf333cb810601fe5
jedifunk <iam@bryceflory.com>
2015-11-23 11:36:08 -0600

    1.1 Updated Magnific to 1.0
    Updated Magnific to 1.0

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # jbfj-lightbox
 Simple Wordpress Wrapper for Magnific Popup
+
+### Changelog
+1.1: November 23, 2015
+- Updated Magnific to 1.0
+- Added Image option
+- Added Gallery option
--- jbfj-lightbox.php
+++ jbfj-lightbox.php
@@ -3,14 +3,14 @@
 	Plugin Name: JBFJ Lightbox
 	Author: Bryce Flory and Chad Sanda
 	Author URI: http://bryceflory.com
-	Version: 1.0
+	Version: 1.1
 */
 //Add Javascript
 add_action('wp_print_scripts', 'jbfj_lightbox_scripts');
 
 function jbfj_lightbox_scripts() {
 	//register
-	wp_register_script('jbfj_lightbox', plugins_url('lib/magnific-popup-v0.9.9.js', __FILE__), array('jquery'));
+	wp_register_script('jbfj_lightbox', plugins_url('lib/jquery.magnific-popup.min-1.0.js', __FILE__), array('jquery'));
 	
 	wp_enqueue_script('jbfj_lightbox');
 	
